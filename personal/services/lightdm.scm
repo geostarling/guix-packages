9778de2f8eb13507d1d70d52eb0767e46ce2bfad
Jiri Spacek <spaceji3@fit.cvut.cz>
2019-05-29 08:05:53 +0200

    lightdm: apply new patch

--- personal/services/lightdm.scm
+++ personal/services/lightdm.scm
@@ -1,3 +1,22 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2019 L  p R n  d n   <guix@lprndn.info>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+
 (define-module (personal services lightdm)
   #:use-module (guix gexp)
   #:use-module (guix records)
@@ -17,58 +36,75 @@
   #:use-module (gnu packages gnome)
   #:use-module (gnu packages xorg)
 
-  #:use-module (personal packages display-managers)
-
   #:export (lightdm-configuration
             lightdm-configuration?
-            lightdm-service-type))
+            lightdm-service-type
+            lightdm-gtk-greeter-configuration
+            lightdm-gtk-greeter-configuration-file))
+
+
+(define-record-type* <lightdm-gtk-greeter-configuration>
+  lightdm-gtk-greeter-configuration make-lightdm-gtk-greeter-configuration
+  lightdm-gtk-greeter-configuration?
+  (theme-name lightdm-gtk-greeter-configuration-theme-name
+              (default "Adwaita"))
+  (icon-theme-name
+   lightdm-gtk-greeter-configuration-icon-theme-name
+   (default "Adwaita"))
+  (cursor-theme-name
+   lightdm-gtk-greeter-configuration-cursor-theme-name
+   (default "Adwaita"))
+  (cursor-size lightdm-gtk-greeter-configuration-cursor-size
+               (default 16))
+  (background lightdm-gtk-greeter-configuration-background
+              (default "")))
+
+(define (lightdm-gtk-greeter-configuration-file config)
+  (mixed-text-file "lightdm-gtk-greeter.conf" "
+[greeter]
+theme-name = "        (lightdm-gtk-greeter-configuration-theme-name config) "
+icon-theme-name = "   (lightdm-gtk-greeter-configuration-icon-theme-name config) "
+cursor-theme-name = " (lightdm-gtk-greeter-configuration-cursor-theme-name config) "
+cursor-theme-size = " (number->string
+                       (lightdm-gtk-greeter-configuration-cursor-size config)) "
+background = "        (lightdm-gtk-greeter-configuration-background config)))
 
 (define-record-type* <lightdm-configuration>
   lightdm-configuration make-lightdm-configuration
   lightdm-configuration?
 
   (lightdm lightdm-configuration-lightdm
-           (default my-lightdm))
-  (user lightdm-configuration-user
-        (default "lightdm"))
-  (greeters-directory lightdm-configuration-greeters-directory
-                      (default "/run/current-system/profile/share/xgreeters"))
+           (default lightdm))
   (sessions-directory lightdm-configuration-sessions-directory
                       (default (string-append
                                 "/run/current-system/profile/share/xsessions"
                                 ":/run/current-system/profile/share/wayland-sessions")))
   (allow-empty-passwords? lightdm-configuration-allow-empty-passwords?
                           (default #f))
-  ;; Seat configuration
-  (greeter-session lightdm-configuration-greeter-session
-                   (default "lightdm-gtk-greeter"))
-  (xserver-command lightdm-configuration-xserver-command
-                   (default (xorg-start-command)))
+  ;; [Seat]
+  (xorg-configuration lightdm-configuration-xorg
+                      (default (xorg-configuration)))
   (session-wrapper lightdm-configuration-session-wrapper
                    (default (xinitrc)))
-  (autologin-user lightdm-configuration-autologin-user
-                  (default ""))
   (default-session-name lightdm-configuration-default-session
     (default ""))
+
+  ;; [Autologin]
+  (autologin-user lightdm-configuration-autologin-user
+                  (default #f))
   (autologin-timeout lightdm-configuration-autologin-timeout
-                     (default ""))
-  ;; lightdm-gtk-greeter specifics
-  ;; Maybe it should have its own service
-  (gtk-greeter-assets lightdm-configuration-gtk-greeter-assets
-                      (default (list adwaita-icon-theme
-                                     gnome-themes-standard)))
-  (gtk-greeter-theme-name lightdm-configuration-gtk-greeter-theme-name
-                          (default "Adwaita"))
-  (gtk-greeter-icon-theme-name
-   lightdm-configuration-gtk-greeter-icon-theme-name
-   (default "Adwaita"))
-  (gtk-greeter-cursor-theme-name
-   lightdm-configuration-gtk-greeter-cursor-theme-name
-   (default "Adwaita"))
-  (gtk-greeter-cursor-size lightdm-configuration-gtk-greeter-cursor-size
-                           (default 16))
-  (gtk-greeter-background lightdm-configuration-gtk-greeter-background
-                          (default "")))
+                     (default #f))
+  ;; [Greeter]
+  (greeter-name lightdm-configuration-greeter-name
+                (default "lightdm-gtk-greeter"))
+  (greeter-package lightdm-configuration-greeter-package
+                   (default lightdm-gtk-greeter))
+  (greeter-assets lightdm-configuration-greeter-assets
+                  (default (list adwaita-icon-theme
+                                 gnome-themes-standard)))
+  (greeter-configuration-file lightdm-configuration-greeter-configuration
+                              (default (lightdm-gtk-greeter-configuration-file
+                                        (lightdm-gtk-greeter-configuration)))))
 
 (define %lightdm-accounts
   (list (user-group (name "lightdm") (system? #t))
@@ -80,33 +116,27 @@
          (home-directory "/var/lib/lightdm")
          (shell (file-append shadow "/sbin/nologin")))))
 
+
 (define (lightdm-configuration-file config)
   (mixed-text-file "lightdm.conf" "
 [LightDM]
-greeter-user = "          (lightdm-configuration-user config) "
-greeters-directory = "    (lightdm-configuration-greeters-directory config) "
-sessions-directory = "    (lightdm-configuration-sessions-directory config) "
+greeter-user = lightdm
+greeters-directory = /run/current-system/profile/share/xgreeters
+sessions-directory = "  (lightdm-configuration-sessions-directory config) "
 
 
 [Seat:*]
-xserver-command = "       (lightdm-configuration-xserver-command config) "
-greeter-session = "       (lightdm-configuration-greeter-session config) "
-user-session = "          (lightdm-configuration-default-session config) "
-autologin-user = "        (lightdm-configuration-autologin-user config) "
-autologin-session = "     (lightdm-configuration-default-session config) "
-autologin-user-timeout = " (lightdm-configuration-autologin-timeout config) "
+xserver-command = "  (xorg-start-command (lightdm-configuration-xorg config)) "
+greeter-session = "       (lightdm-configuration-greeter-name config) "
+user-session = "          (lightdm-configuration-default-session config)
+(if (lightdm-configuration-autologin-user config) (string-append "
+autologin-user = " (lightdm-configuration-autologin-user config)) "")
+(if (string-null? (lightdm-configuration-default-session config)) ""
+    (string-append "
+autologin-session = " (lightdm-configuration-default-session config)))
+(if (lightdm-configuration-autologin-timeout config) (string-append "
+autologin-user-timeout = " (lightdm-configuration-autologin-timeout config)) "") "
 session-wrapper = " (lightdm-configuration-session-wrapper config)))
-
-
-(define (lightdm-gtk-greeter-configuration-file config)
-  (mixed-text-file "lightdm-gtk-greeter.conf" "
-[greeter]
-theme-name = "        (lightdm-configuration-gtk-greeter-theme-name config) "
-icon-theme-name = "   (lightdm-configuration-gtk-greeter-icon-theme-name config) "
-cursor-theme-name = " (lightdm-configuration-gtk-greeter-cursor-theme-name config) "
-cursor-theme-size = " (number->string
-                       (lightdm-configuration-gtk-greeter-cursor-size config)) "
-background = "        (lightdm-configuration-gtk-greeter-background config)))
 
 
 (define (lightdm-pam-service config)
@@ -194,8 +224,10 @@ background = "        (lightdm-configuration-gtk-greeter-background config)))
 (define (lightdm-etc-service config)
   (list `("xdg/lightdm/lightdm.conf.d/lightdm.conf"
           ,(lightdm-configuration-file config))
-        `("xdg/lightdm/lightdm-gtk-greeter.conf"
-          ,(lightdm-gtk-greeter-configuration-file config))))
+        `(,(string-append "xdg/lightdm/"
+                          (computed-file-name
+                           (lightdm-configuration-greeter-configuration config)))
+          ,(lightdm-configuration-greeter-configuration config))))
 
 (define (lightdm-pam-services config)
   (list (lightdm-pam-service config)
@@ -203,15 +235,16 @@ background = "        (lightdm-configuration-gtk-greeter-background config)))
         (lightdm-autologin-pam-service)))
 
 (define (lightdm-profile-service config)
-  (append (list my-lightdm-gtk-greeter my-lightdm)
-          (lightdm-configuration-gtk-greeter-assets config)))
+  (append (list lightdm
+                (lightdm-configuration-greeter-package config))
+          (lightdm-configuration-greeter-assets config)))
 
 (define (lightdm-activation-service config)
   (with-imported-modules '((guix build utils))
     #~(begin
         (use-modules (guix build utils))
         (define %user
-          (getpw #$(lightdm-configuration-user config)))
+          (getpw "lightdm"))
         (let ((directory "/var/lib/lightdm-data"))
           (mkdir-p directory)
           (chown directory (passwd:uid %user) (passwd:gid %user))))))
@@ -234,4 +267,7 @@ background = "        (lightdm-configuration-gtk-greeter-background config)))
                                      (const %lightdm-accounts))
                   (service-extension profile-service-type
                                      lightdm-profile-service)))
-                (default-value (lightdm-configuration))))
+                (default-value (lightdm-configuration))
+                (description "Return a service that spawns the LightDM graphical login
+ manager.")))
+
