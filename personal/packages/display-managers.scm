71df8328e779e38a31a23a4ba83d4460bb37d0c9
Jiri Spacek <spaceji3@fit.cvut.cz>
2019-05-30 22:17:26 +0200

    slick-greeter: lightdm-with-vala: add new packages

--- personal/packages/display-managers.scm
+++ personal/packages/display-managers.scm
@@ -20,14 +20,20 @@
   #:use-module (guix download)
   #:use-module (guix build-system cmake)
   #:use-module (guix build-system gnu)
+  #:use-module (guix build-system glib-or-gtk)
   #:use-module (guix packages)
   #:use-module (guix utils)
+  #:use-module (srfi srfi-1)
   #:use-module (gnu packages)
   #:use-module (gnu packages admin)
+  #:use-module (gnu packages base)
   #:use-module (gnu packages fontutils)
   #:use-module (gnu packages freedesktop)
   #:use-module (gnu packages gl)
+  #:use-module (gnu packages autotools)
   #:use-module (gnu packages glib)
+  #:use-module (gnu packages libcanberra)
+  #:use-module (gnu packages display-managers)
   #:use-module (gnu packages gnome)
   #:use-module (gnu packages gnupg)
   #:use-module (gnu packages gtk)
@@ -41,104 +47,70 @@
   #:use-module (gnu packages xfce)
   #:use-module (gnu packages xorg))
 
-(define-public my-lightdm
+(define-public lightdm-with-vala
   (package
-    (name "lightdm")
-    (version "1.24.0")
+    (inherit lightdm)
+    (name "lightdm-with-vala")
+    (version "1.30.0")
     (source (origin
-              (method url-fetch)
-              (uri (string-append "https://launchpad.net/lightdm/"
-                                  (version-major+minor version) "/"
-                                  version "/+download/lightdm-"
-                                  version ".tar.xz"))
-              (sha256
-               (base32
-                "18j33bm54i8k7ncxcs69zqi4105s62n58jrydqn3ikrb71s9nl6d"))))
-    (build-system gnu-build-system)
+             (method url-fetch)
+             (uri (string-append "https://github.com/CanonicalLtd/lightdm/releases/download/"
+                                 version "/lightdm-"
+                                 version ".tar.xz"))
+             (sha256
+              (base32
+               "158zb2d0v1309a8v19hh32y4yj3v6yg4yg6m0l7v59d3a2b7f651"))))
     (arguments
-     '(#:tests? #f
-       #:parallel-tests? #f ; fails when run in parallel
-       #:configure-flags
-       (list "--localstatedir=/var")
-       #:phases
-       (modify-phases %standard-phases
-         (add-after 'unpack 'fix-paths
-           (lambda _
-             (substitute* "src/shared-data-manager.c"
-               (("/bin/rm") (which "rm")))
-             (substitute* '("data/users.conf"
-                            "common/user-list.c")
-               (("/bin/false") (which "false"))
-               (("/usr/sbin/nologin") (which "nologin")))
-             (substitute* "src/seat.c"
-               (("/bin/sh") (which "sh")))
-             #t))
-         (add-after 'unpack 'disable-broken-tests
-           (lambda _
-             (substitute* "tests/Makefile.in"
-               (("test-sessions-gobject ") "")
-               ((" test-sessions-python ") " "))
-             #t))
-         (add-before 'check 'pre-check
-           ;; Run test-suite under a dbus session.
-           (lambda* (#:key inputs #:allow-other-keys)
-             (wrap-program "tests/src/test-python-greeter"
-               `("PYTHONPATH"      ":" prefix (,(getenv "PYTHONPATH")))
-               `("GI_TYPELIB_PATH" ":" prefix (,(getenv "GI_TYPELIB_PATH"))))
-
-             ;; Avoid printing locale warnings, which trip up the text
-             ;; matching tests.
-             (unsetenv "LC_ALL")
-             #t)))))
+     (substitute-keyword-arguments (package-arguments lightdm)
+      ((#:configure-flags flags ''())
+       `(cons* "--enable-vala"
+               ,flags))))
     (inputs
-     `(("audit" ,audit)
-       ("linux-pam" ,linux-pam)
-       ("shadow" ,shadow)                         ;for sbin/nologin
-       ("libgcrypt" ,libgcrypt)
-       ("libxcb" ,libxcb)))
-    (native-inputs
-     `(("gobject-introspection" ,gobject-introspection)
-       ("pkg-config" ,pkg-config)
-       ("itstool" ,itstool)
-       ("intltool" ,intltool)
-       ;; For tests
-       ("dbus" ,dbus)
-       ("python" ,python-2)
-       ("python-pygobject" ,python2-pygobject)))
-    ;; Required by liblightdm-gobject-1.pc.
-    (propagated-inputs
-     `(("glib" ,glib)
-       ("libx11" ,libx11)
-       ("libxklavier" ,libxklavier)))
-    (home-page "https://www.freedesktop.org/wiki/Software/LightDM/")
-    (synopsis "Lightweight display manager")
-    (description "The Light Display Manager (LightDM) is a cross-desktop
-display manager which supports different greeters.")
-    (license license:gpl3+)))
+     (cons `("vala" ,vala) (package-inputs lightdm)))))
 
-
-(define-public my-lightdm-gtk-greeter
+(define-public slick-greeter
   (package
-    (name "lightdm-gtk-greeter")
-    (version "2.0.2")
+    (name "slick-greeter")
+    (version "1.2.4")
     (source (origin
               (method url-fetch)
               (uri (string-append
-                    "https://launchpad.net/lightdm-gtk-greeter/"
-                    (version-major+minor version) "/" version
-                    "/+download/lightdm-gtk-greeter-" version ".tar.gz"))
+                    "https://github.com/linuxmint/slick-greeter/archive/" version ".tar.gz"))
               (sha256
                (base32
-                "1436sdm83xqhxyr1rzqxhsl8if2xmidlvb341xcv6dv83lyxkrlf"))))
-    (build-system gnu-build-system)
+                "1wislxdld8z94ym9vk6a35manwipiccgk6n9dsp6wrgn34r8f3z0"))))
+    (build-system glib-or-gtk-build-system)
+    (arguments
+     '(#:tests? #f
+       #:phases
+       (modify-phases %standard-phases
+                      (replace 'bootstrap
+                       (lambda _
+                         ;; The autogen.sh script in gnome-common will run ./configure
+                         ;; by default, which is problematic because source shebangs
+                         ;; have not yet been patched.
+                         (setenv "NOCONFIGURE" "t")
+                         (zero? (system* "sh" "autogen.sh")))))))
     (native-inputs
-     `(("exo" ,exo)
+     `(("which" ,which)
+       ("pkg-config" ,pkg-config)
        ("intltool" ,intltool)
-       ("pkg-config" ,pkg-config)))
+       ("vala" ,vala)
+       ("gobject-introspection" ,gobject-introspection)
+       ("gnome-common" ,gnome-common)
+       ("automake" ,automake)
+       ("autoconf" ,autoconf)))
     (inputs
-     `(("lightdm" ,my-lightdm)
-       ("gtk+" ,gtk+)))
-    (synopsis "GTK+ greeter for LightDM")
+     `(("glib" ,glib)
+       ("gtk+" ,gtk+)
+       ("lightdm" ,lightdm-with-vala)
+       ("freetype" ,freetype)
+       ("cairo" ,cairo)
+       ("libcanberra" ,libcanberra)
+       ("pixman" ,pixman)
+       ("libx11" ,libx11)
+       ("libxext" ,libxext)))
+    (synopsis "Slick greeter for LightDM")
     (home-page "https://launchpad.net/lightdm-gtk-greeter")
     (description "This package provides a LightDM greeter implementation using
 GTK+, lets you select a desktop session and log in to it.")
