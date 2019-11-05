8d742ac5c32501df493e8a9bed534819c1b2ca76
Jiri Spacek <spacekj3@gmail.com>
2019-11-05 21:07:24 +0100

    tvheadend service: fix

--- personal/services/tv.scm
+++ personal/services/tv.scm
@@ -78,9 +78,6 @@
   (group
    (string "tvheadend")
    "Group who will run the Tvheadend daemon.")
-  (pid-file
-   (string  "/var/run/tvheadend/tvheadend.pid")
-   "Name of PID file.")
   (satip-bind-address
    (string "")
    "Bind address for SAT>IP server")
@@ -116,7 +113,11 @@
    "Specify User-Agent header for the http client.")
   (use-xspf-playlist?
    (boolean #f)
-   "Use XSPF playlist instead of M3U."))
+   "Use XSPF playlist instead of M3U.")
+  (acl-disable?
+   (boolean #f)
+   "Disable all access control checks."))
+
 
 (define (tvheadend-account config)
   "Return the user accounts and user groups for CONFIG."
@@ -143,13 +144,11 @@
                         (mkdir-p directory)
                         (chown directory (passwd:uid user) (passwd:gid user))
                         (chmod directory #o750))
-                    (list (dirname #$(tvheadend-configuration-pid-file config))
-                          #$(tvheadend-configuration-config-path config)))))))
+                    (list #$(tvheadend-configuration-config-path config)))))))
 
 (define (tvheadend-shepherd-service config)
   "Return a <shepherd-service> for tvheadend with CONFIG."
   (let* ((tvheadend   (tvheadend-configuration-package config))
-         (pid-file    (tvheadend-configuration-pid-file config))
          (user        (tvheadend-configuration-user config))
          (group       (tvheadend-configuration-group config)))
     (list (shepherd-service
@@ -158,8 +157,7 @@
            (requirement '(networking))
            (start #~(make-forkexec-constructor
                      (list (string-append #$tvheadend "/bin/tvheadend")
-                           "--config" #$(tvheadend-configuration-config-path config)
-                           "--pid" #$(tvheadend-configuration-pid-file config))
+                           "--config" #$(tvheadend-configuration-config-path config))
                            ;; TODO:"--adapters" #$@(tvheadend-adapters config)
                            ;; TODO: every other config opt/
                      #:user #$user
