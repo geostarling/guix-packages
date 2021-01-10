f84069b7774affa828316bb8b020ea721393f983
Jiri Spacek <spacekj3@gmail.com>
2021-01-10 18:09:58 +0100

    lokke: add package

---
+++ personal/packages/scheme.scm
@@ -0,0 +1,116 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2019 Ivan Petkov <ivanppetkov@gmail.com>
+;;; Copyright © 2019, 2020 Efraim Flashner <efraim@flashner.co.il>
+;;; Copyright © 2019 Nicolas Goaziou <mail@nicolasgoaziou.fr>
+;;; Copyright © 2019 Giacomo Leidi <goodoldpaul@autistici.org>
+;;; Copyright © 2019 Tobias Geerinckx-Rice <me@tobias.gr>
+;;; Copyright © 2019 John Soo <jsoo1@asu.edu>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (personal packages scheme)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix git-download)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (gnu packages guile)
+  #:use-module (gnu packages version-control)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages gettext)
+  #:use-module (gnu packages pcre)
+  #:use-module (gnu packages man)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages bash))
+
+
+(define-public lokke
+  (let ((commit "b9aa7e3dfd3b4134df0b72e82174adbe165dbe53"))
+    (package
+     (name "lokke")
+     (version "0.0.0")
+     (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+              (url "https://git.sr.ht/~rlb/lokke")
+              (commit commit)))
+        (file-name (git-file-name "lokke" version))
+        (sha256
+         (base32
+          "14bzl53n1wqjin3crd3bdx3h6r89daqwpgrd02693yvgicz39qs5"))))
+     (build-system gnu-build-system)
+     (arguments
+      `(#:phases
+        (modify-phases %standard-phases
+          (add-after 'unpack 'patch-setup-shebangs
+            (lambda _
+              (substitute* "dev/refresh"
+                (("/usr/bin/env bash") (which "bash")))
+              (substitute* "dev/gen-module-paths"
+                (("/usr/bin/env bash") (which "bash")))
+              (substitute* "gen-makefile"
+                (("/usr/bin/env guile") (which "guile")))
+              (substitute* "guile.in"
+                (("guile-@GUILE_EFFECTIVE_VERSION@") (which "guile")))
+              ;; (substitute* "guile.in"
+              ;;              (("libs\"") (string-append "libs\"\nexport XDG_CACHE_HOME=" "/tmp/ccache" "\n set -x"
+              ;;                                         "\nexport HOME=" "/tmp/ccache" "\n"
+              ;;                                         "\n guile -c '(use-modules (system base compile)) (display %compile-fallback-path )'")))
+              ;;                           ;
+
+              ;; (substitute* "mod/lokke/ns.scm"
+              ;;              ((".*define debug-ns.*") "(define debug-ns? #t)")
+              ;;              ((".*found src.*") "(dbgf \"found src: ~s\n\" %load-compiled-extensions)"))
+              (substitute* "Makefile.am.in"
+                (("COLUMNS=80.*") "COLUMNS=80 man -l $^ > /dev/null"))
+              #t))
+          (add-before 'bootstrap 'invoke-setup-script
+            (lambda _
+              (invoke "git" "init")
+              (invoke "git" "add" ".")
+              (invoke "git" "config" "user.email" "you@example.com")
+              (invoke "git" "config" "user.name" "Your Name")
+              (invoke "git" "commit" "-m" "dummy")
+              (invoke (which "sh") "./setup")))
+          (replace 'bootstrap
+            (lambda _
+              (invoke "autoreconf" "-vfi")))
+          (add-before 'configure 'setenv-vars
+            (lambda _
+              ;; (setenv "GUILE_LOAD_PATH"
+              ;;  (string-append (getcwd) "/mod"))
+              ;; (setenv "GUILE_LOAD_COMPILED_PATH"
+              ;;         (string-append "" (getcwd) "/mod"))
+              (setenv "XDG_CACHE_HOME" "/tmp/ccache"))))))
+              ;; (setenv "LTDL_LIBRARY_PATH"
+              ;;  (string-append "" (getcwd) "/lib/.libs")))))))
+     (native-inputs
+      `(("autoconf" ,autoconf)
+        ("automake" ,automake)
+        ("pkg-config" ,pkg-config)
+        ("bash" ,bash)  ; for ./setup script
+        ("git" ,git)  ; for ./setup script
+        ("gettext" ,gettext-minimal)
+        ("libtool" ,libtool)
+        ("man-db" ,man-db)))
+     (inputs
+      `(("guile" ,guile-3.0)
+        ("pcre2" ,pcre2)))
+     (synopsis "NGINX module for Lua programming language support")
+     (description "This NGINX module provides a scripting support with Lua
+programming language.")
+     (license (list license:unlicense license:expat))
+     (home-page "https://github.com/BurntSushi/ripgrep"))))
