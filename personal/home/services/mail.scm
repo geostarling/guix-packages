190917132c7cb267b453dee468e2417abb60ae94
Jiri Spacek <spacekj3@gmail.com>
2021-12-28 13:20:27 +0100

    Add fork of https://git.sr.ht/~abcdw/rde

---
+++ personal/home/services/mail.scm
@@ -0,0 +1,406 @@
+(define-module (personal home services mail)
+  #:use-module (gnu home services)
+  #:use-module (personal home utils)
+  #:use-module (gnu home services shepherd)
+  #:use-module (gnu packages mail)
+  #:use-module (gnu services configuration)
+
+  #:use-module (srfi srfi-1)
+  #:use-module (ice-9 match)
+
+  #:use-module (guix packages)
+  #:use-module (guix gexp)
+  #:use-module (guix diagnostics)
+  #:use-module (guix i18n)
+  #:use-module ((guix import utils) #:select (flatten))
+
+  #:export (home-isync-service-type
+            home-isync-configuration
+
+            home-notmuch-service-type
+            home-notmuch-configuration
+            home-notmuch-extension
+
+            home-l2md-service-type
+            home-l2md-configuration
+            l2md-repo
+            l2md-repo-name
+            l2md-repo-urls
+            l2md-repo-maildir
+            l2md-repo-pipe
+            l2md-repo-initial-import
+            l2md-repo-sync-enabled?))
+
+;;; Commentary:
+;;;
+;;; This modules contains mail-related services.
+;;;
+;;; Code:
+
+
+;;;
+;;; Isync.
+;;;
+
+(define (serialize-isync-config field-name val)
+  (define (serialize-term term)
+    (match term
+      ((? symbol? e) (symbol->string e))
+      ((? number? e) (format #f "~a" e))
+      ((? string? e) (format #f "~s" e))
+      (e e)))
+  (define (serialize-item entry)
+    (match entry
+      ((? gexp? e) e)
+      ((? list lst)
+       #~(string-join '#$(map serialize-term lst)))))
+
+  #~(string-append #$@(interpose (map serialize-item val) "\n" 'suffix)))
+
+(define-configuration/no-serialization home-isync-configuration
+  (package
+   (package isync)
+   "isync package to use.")
+  (xdg-flavor?
+   (boolean #t)
+   "Whether to use the @file{$XDG_CONFIG_HOME/isync/mbsyncrc}
+configuration file or not.  If @code{#t} creates a wrapper for mbsync
+binary.")
+  (config
+   (list '())
+   "AList of pairs, each pair is a String and String or Gexp."))
+
+(define (add-isync-package config)
+  (define wrapper-gexp
+    #~(system
+       (string-join
+        (cons
+         #$(file-append (home-isync-configuration-package config)
+                        "/bin/mbsync")
+         (if (or (member "-c" (command-line))
+                 (member "--config" (command-line)))
+             (cdr (command-line))
+             (append
+              (list "--config"
+                    "${XDG_CONFIG_HOME:-$HOME/.config}/isync/mbsyncrc")
+              (cdr (command-line))))))))
+  (list
+   (if (home-isync-configuration-xdg-flavor? config)
+       (wrap-package
+        (home-isync-configuration-package config)
+        "mbsync" wrapper-gexp)
+       (home-isync-configuration-package config))))
+
+(define (add-isync-configuration config)
+  `((,(if (home-isync-configuration-xdg-flavor? config)
+          "config/isync/mbsyncrc"
+          "mbsyncrc")
+     ,(mixed-text-file
+       "mbsyncrc"
+       (serialize-isync-config #f (home-isync-configuration-config config))))))
+
+(define (home-isync-extensions cfg extensions)
+  (home-isync-configuration
+   (inherit cfg)
+   (config (append (home-isync-configuration-config cfg) extensions))))
+
+(define home-isync-service-type
+  (service-type (name 'home-isync)
+                (extensions
+                 (list (service-extension
+                        home-profile-service-type
+                        add-isync-package)
+                       (service-extension
+                        home-files-service-type
+                        add-isync-configuration)))
+                (compose concatenate)
+                (extend home-isync-extensions)
+                (default-value (home-isync-configuration))
+                (description "Install and configure isync.")))
+
+(define (list-of-gexps? lst)
+  (and (list? lst) (every gexp? lst)))
+
+(define (generate-home-isync-documentation)
+  (generate-documentation
+   `((home-isync-configuration
+      ,home-isync-configuration-fields))
+   'home-isync-configuration))
+
+
+;;;
+;;; Notmuch.
+;;;
+
+(define-configuration/no-serialization home-notmuch-configuration
+  (package
+   (package notmuch)
+   "notmuch package to use.")
+  (xdg-flavor?
+   (boolean #t)
+   "Whether to use the {$XDG_CONFIG_HOME/notmuch/default/config}
+configuration file or not.")
+  (config
+   (ini-config '())
+   "AList of pairs, each pair is a String and String or Gexp.")
+  (pre-new
+   (list-of-gexps '())
+   "List of gexp to add in @file{pre-new} hook. Read @code{man
+notmuch-hooks} for more information.")
+  (post-new
+   (list-of-gexps '())
+   "List of gexp to add in @file{post-new} hook. Read @code{man
+notmuch-hooks} for more information.")
+  (post-insert
+   (list-of-gexps '())
+   "List of gexp to add in @file{post-insert} hook. Read @code{man
+notmuch-hooks} for more information."))
+
+(define-configuration/no-serialization home-notmuch-extension
+  (config
+   (ini-config '())
+   "AList of pairs, each pair is a String and String or Gexp.")
+  (pre-new
+   (list-of-gexps '())
+   "List of gexp to add in @file{pre-new} hook. Read @code{man
+notmuch-hooks} for more information.")
+  (post-new
+   (list-of-gexps '())
+   "List of gexp to add in @file{post-new} hook. Read @code{man
+notmuch-hooks} for more information.")
+  (post-insert
+   (list-of-gexps '())
+   "List of gexp to add in @file{post-insert} hook. Read @code{man
+notmuch-hooks} for more information."))
+
+(define (add-notmuch-package config)
+  (list (home-notmuch-configuration-package config)))
+
+(define (add-notmuch-configuration config)
+  (define (serialize-field key val)
+    (let ((val (cond
+                ((list? val) (string-join (map maybe-object->string val) ";"))
+                (else val))))
+      (format #f "~a=~a\n" key val)))
+
+  (define (filter-fields field)
+    (filter-configuration-fields home-notmuch-configuration-fields
+                                 (list field)))
+
+  (define (hook-file hook gexps)
+    (list (string-append "config/notmuch/default/hooks/" hook)
+          (program-file (string-append "notmuch-" hook) #~(begin #$@gexps))))
+
+  (define (get-hook hook)
+    (let* ((field-obj (car (filter-fields (string->symbol hook))))
+           (gexps ((configuration-field-getter field-obj) config)))
+      (if (not (null? gexps))
+          (hook-file hook gexps)
+          '())))
+
+  (remove null?
+  `(,@(map get-hook '("pre-new" "post-new" "post-insert"))
+    (,(if (home-notmuch-configuration-xdg-flavor? config)
+          "config/notmuch/default/config"
+          "notmuch-config")
+     ,(mixed-text-file
+       "notmuch-config"
+       (generic-serialize-ini-config
+        #:serialize-field serialize-field
+        #:fields (home-notmuch-configuration-config config)))))))
+
+(define (home-notmuch-extensions cfg extensions)
+  (home-notmuch-configuration
+   (inherit cfg)
+   (config
+    (append (home-notmuch-configuration-config cfg)
+            (append-map home-notmuch-extension-config extensions)))
+   (pre-new
+    (append (home-notmuch-configuration-pre-new cfg)
+            (append-map home-notmuch-extension-pre-new extensions)))
+   (post-new
+    (append (home-notmuch-configuration-post-new cfg)
+            (append-map home-notmuch-extension-post-new extensions)))
+   (post-insert
+    (append (home-notmuch-configuration-post-insert cfg)
+            (append-map home-notmuch-extension-post-insert extensions)))))
+
+(define home-notmuch-service-type
+  (service-type (name 'home-notmuch)
+                (extensions
+                 (list (service-extension
+                        home-profile-service-type
+                        add-notmuch-package)
+                       (service-extension
+                        home-files-service-type
+                        add-notmuch-configuration)))
+                (compose identity)
+                (extend home-notmuch-extensions)
+                (default-value (home-notmuch-configuration))
+                (description "Install and configure notmuch.")))
+
+(define (generate-home-notmuch-documentation)
+  (generate-documentation
+   `((home-notmuch-configuration
+      ,home-notmuch-configuration-fields))
+   'home-notmuch-configuration))
+
+
+;;;
+;;; L2md.
+;;;
+
+(define (string-or-list-of-strings? val)
+  (or (string? val) (list-of-strings? val)))
+
+(define-maybe/no-serialization string)
+(define-maybe/no-serialization string-or-gexp)
+
+(define-configuration/no-serialization l2md-repo
+  (name
+   (string)
+   "The name of the public-inbox repository.")
+  (urls
+   (string-or-list-of-strings)
+   "A list of URLs to fetch the public-inbox repository from.")
+  (maildir
+   (maybe-string 'disabled)
+   "The maildir corresponding to the public-inbox repository.  This is
+optional, an external MDA like Procmail can be used instead to filter
+the messages, see the @code{pipe} field.")
+  (pipe
+   (maybe-string-or-gexp 'disabled)
+   "A command to pipe the messages to for further filtering.  This is
+mutually exclusive with the @code{maildir} field.")
+  (initial-import
+   (integer 0)
+   "The number of messages to import initially, if @code{0}, import all
+the messages.")
+  (sync-enabled?
+   (boolean #t)
+   "Whether to sync this repository or not."))
+
+(define list-of-l2md-repos? (list-of l2md-repo?))
+
+(define-configuration/no-serialization home-l2md-configuration
+  (package
+    (package l2md)
+    "The L2md package to use.")
+  (autostart?
+   (boolean #f)
+   "Whether to autostart L2md on login.")
+  (period
+   (integer 180)
+   "The number of seconds between each round of fetching Git
+repositories.")
+  (oneshot
+   (integer 0)
+   "@code{0} to watch for new emails every PERIOD seconds, @code{1} to
+sync once and exit.")
+  (maildir
+   (maybe-string 'disabled)
+   "The maildir to which messages should be delivered.  This can also be
+set on a per-list basis using the using the @code{maildir} field in
+the @code{<l2md-repo>} record.")
+  (pipe
+   (maybe-string-or-gexp 'disabled)
+   "A command to pipe the messages to for further filtering.  This is
+mutually exclusive with the @code{maildir} field.  This can also be
+set on a per-list basis using the @code{<l2md-repo>} record.")
+  (base
+   (string "${XDG_STATE_HOME:-$HOME}/l2md")
+   "The directory where L2md stores Git repositories and other
+metadata.")
+  (repos
+   (list-of-l2md-repos '())
+   "List of @code{l2md-repo} records, representing the configuration for
+a particular public-inbox repository."))
+
+(define (serialize-l2md-configuration config)
+  (define (serialize-field field-name val)
+    (let ((val (cond
+                ((boolean? val) (if val "1" "0"))
+                (else (maybe-object->string val)))))
+      (if (string= val "")
+          '()
+          (list "\t" (object->snake-case-string field-name) " = " val "\n"))))
+
+  (define (l2md-repo->alist repos)
+    (match repos
+      (($ <l2md-repo> _ name urls maildir pipe initial-import sync-enabled?)
+       (begin
+         `(repo ,name
+                (,@(map (lambda (url)
+                          `(url . ,url))
+                        (maybe-list urls))
+                 ,@(optional (not (eq? maildir 'disabled)) `((maildir . ,maildir)))
+                 ,@(optional (not (eq? pipe 'disabled)) `((pipe . ,pipe)))
+                 (initial-import . ,initial-import)
+                 (sync-enabled . ,sync-enabled?)))))))
+
+  (match config
+    (($ <home-l2md-configuration> _ package autostart? period oneshot maildir pipe base repos)
+     (begin
+       (generic-serialize-git-ini-config
+        #:combine-ini (compose flatten list)
+        #:combine-alist append
+        #:combine-section-alist cons*
+        #:serialize-field serialize-field
+        #:fields
+        `((general
+           ((period . ,period)
+            ,@(optional (not (eq? maildir 'disabled)) `((maildir . ,maildir)))
+            ,@(optional (not (eq? pipe 'disabled)) `((pipe . ,pipe)))
+            (oneshot . ,oneshot)
+            (base . ,base)))
+          ,@(map l2md-repo->alist repos)))))))
+
+(define (l2md-files-service config)
+  `(("l2mdconfig"
+     ,(apply mixed-text-file
+             "l2mdconfig"
+             (serialize-l2md-configuration config)))))
+
+(define l2md-shepherd-service
+  (match-lambda
+    (($ <home-l2md-configuration> _ package autostart?)
+     (optional autostart?
+               (list (shepherd-service
+                      (documentation
+                       "L2md service for downloading public-inbox archives.")
+                      (provision '(l2md))
+                      (start #~(make-forkexec-constructor
+                                (list #$(file-append package "/bin/l2md"))
+                                #:log-file (string-append
+                                            (or (getenv "XDG_LOG_HOME")
+                                                (string-append
+                                                 (getenv "HOME")
+                                                 "/.local/var/log"))
+                                            "/l2md.log")))
+                      (stop #~(make-kill-destructor))))))))
+
+(define (l2md-profile-service config)
+  (list (home-l2md-configuration-package config)))
+
+(define home-l2md-service-type
+  (service-type (name 'home-l2md)
+                (extensions
+                 (list (service-extension
+                        home-files-service-type
+                        l2md-files-service)
+                       (service-extension
+                        home-shepherd-service-type
+                        l2md-shepherd-service)
+                       (service-extension
+                        home-profile-service-type
+                        l2md-profile-service)))
+                (description "Install and configure L2md.")))
+
+(define (generate-home-l2md-documentation)
+  (generate-documentation
+   `((home-l2md-configuration
+      ,home-l2md-configuration-fields
+      (l2md-repo l2md-repo))
+     (l2md-repo ,l2md-repo-fields))
+   'home-l2md-configuration))
+
---
+++ personal/home/services/ssh.scm
@@ -0,0 +1,259 @@
+(define-module (personal home services ssh)
+  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-9)
+  #:use-module (ice-9 match)
+  #:use-module (gnu home services)
+  #:use-module (personal home utils)
+  #:use-module (gnu services configuration)
+  #:use-module (gnu packages ssh)
+  #:use-module (guix packages)
+  #:use-module (guix import utils)
+  #:use-module (guix i18n)
+  #:use-module (guix diagnostics)
+  #:use-module (guix gexp)
+  #:use-module (guix monads)
+  #:use-module (guix records)
+
+  #:export (home-ssh-service-type
+            home-ssh-configuration
+	    home-ssh-configuration?
+            ssh-host
+            ssh-match))
+
+;;; Commentary:
+;;;
+;;; Service to install and configure SSH.
+;;;
+;;; Code:
+
+(define (uglify-field-name field-name)
+  "Convert symbol FIELD-NAME to an upper camel case string.
+@code{symbol-name} => \"@code{SymbolName}\"."
+  (let ((str (symbol->string field-name)))
+    (apply string-append (map string-capitalize (string-split str #\-)))))
+
+(define* (serialize-field field-name val #:key (toplevel? #f))
+  (cond
+   ((boolean? val) (serialize-boolean field-name val))
+   ((list? val) (serialize-list field-name val #:toplevel? toplevel?))
+   (else 
+    (let ((field-name (uglify-field-name field-name)))
+      (cond
+       ((or (member field-name '("Host" "Match"))
+            toplevel?)
+        #~(format #f "\n~a ~a\n" #$field-name #$val))
+       (else
+        #~(format #f "  ~a ~a\n" #$field-name #$val)))))))
+
+(define* (serialize-list field-name val #:key (toplevel? #f))
+  (if (null? val)
+      ""
+      #~(string-append
+         #$(if toplevel? "" "  ")
+         #$(if field-name (uglify-field-name field-name) "")
+         #$@(map (lambda (val)
+                   #~(format #f " ~a" #$val))
+                  val)
+          "\n")))
+
+(define* (serialize-alist field-name val #:key (toplevel? #f))
+  #~(string-append
+     #$@(map (match-lambda
+            ((field-name . val)
+             (serialize-field field-name val #:toplevel? toplevel?)))
+          val)))
+
+(define (serialize-extra-config field-name val)
+  (define serialize-extra-config-entry
+    (match-lambda
+      ((host name alist)
+       (list
+        (serialize-field host name)
+        (serialize-alist #f alist)))))
+  #~(string-append #$@(append-map serialize-extra-config-entry val)))
+
+(define (serialize-boolean field-name val)
+  (serialize-field field-name (boolean->yes-or-no val)))
+
+(define serialize-string serialize-field)
+(define (list-of-ssh-host-or-ssh-match? val)
+  (list-of (lambda (val)
+            (or (ssh-host? val)
+                (ssh-match? val)))))
+
+(define-enum ssh-match-keywords
+  '(all canonical final exec host originalhost user localuser))
+
+(define %ssh-standalone-keywords
+  '(all canonical final))
+
+(define match-block?
+  (match-lambda
+    ((keyword rest ...)
+     (ssh-match-keywords? keyword))))
+
+(define-configuration/no-serialization ssh-host
+  (host
+   (string)
+   "A pattern to match one or multiple hosts.")
+  (options
+   (alist)
+   "An association list key and value pairs that contain the
+configuration options for the host.  This has the same format as the
+@code{default-options} field in @code{home-ssh-configuration}."))
+
+(define-configuration/no-serialization ssh-match
+  (match
+   (match-block)
+   "A list where the first element is one of @code{ssh-match-keywords}
+and the rest of the elements are arguments for the keyword.")
+  (options
+   (alist)
+   "An association list key and value pairs that contain the
+configuration options for the matched hosts.  This has the same format
+as the @code{default-options} field in @code{home-ssh-configuration}."))
+
+(define serialize-ssh-host
+  (match-lambda
+    (($ <ssh-host> _ host options)
+     #~(string-append
+      #$(serialize-field 'host host)
+      #$(serialize-alist #f options)))))
+
+(define serialize-ssh-match
+  (match-lambda
+    (($ <ssh-match> _ match options)
+     #~(string-append
+        #$(serialize-field
+           'match
+           (if (ssh-match-keywords? (car match))
+               #~(format #f "~a~a"
+                         '#$(car match)
+                         #$(serialize-list #f (cdr match) #:toplevel? #t))
+               (raise (formatted-message
+                       (G_ "Match keyword must be one of the following ~a")
+                       ssh-match-keywords))))
+        #$(serialize-alist #f options)))))
+
+(define (serialize-list-of-ssh-host-or-ssh-match field-name val)
+  #~(string-append
+         #$@(map (lambda (entry)
+                (if (ssh-host? entry)
+                    (serialize-ssh-host entry)
+                    (serialize-ssh-match entry)))
+              val)))
+
+(define-configuration home-ssh-configuration
+  (package
+    (package openssh)
+    "The SSH package to use.")
+  (default-host
+   (string "*")
+   "The name of the default host."
+   (lambda (field-name val) (serialize-field 'host val)))
+  (user-known-hosts-file
+   (list-of-strings '("~/.ssh/known_hosts"))
+   "One or more files to use for the user host key database."
+   serialize-list)
+  (forward-agent
+   (boolean #f)
+   "Whether the connection to the authentication agent will be forwarded
+to the remote machine.")
+  ;; TODO: Maybe we could add fields for some enums?
+  ;; (AddressFamily, FingerprintHash)
+  (default-options
+    (alist '())
+    "Configuration options for the default host.  This should be an
+associative list representing a key-value pair.  A configuration like this:
+
+@lisp
+(home-ssh-configuration
+  (host \"*\")
+  (default-options
+    '((add-keys-to-agent . #t)
+      (address-family . \"inet\"))))
+@end lisp
+
+would turn into this:
+
+@example
+Host *
+  AddKeysToAgent yes
+  AddressFamily inet
+@end example")
+  (toplevel-options
+   (alist '())
+   "Association list of toplevel configuration options.  The configuration below:
+
+@lisp
+(home-ssh-configuration
+  (toplevel-options
+    '((include . \"/some/path/to/file\"))))
+@end lisp
+
+would this:
+
+@example
+Include /some/path/to/file
+@end example"
+   (lambda (field-name val)
+     (serialize-alist field-name val #:toplevel? #t)))
+  (extra-config
+   (list-of-ssh-host-or-ssh-match '())
+   "List of configurations for other hosts.  Something like this:
+
+@lisp
+(home-ssh-configuration
+ (extra-config
+  (list (ssh-host \"savannah\"
+                  '((compression . #f)
+                    (ciphers . (\"3des-cbc\" \"aes256-ctr\"))
+                    (identity-file . \"~/.ssh/keys.d/id_rsa\")
+                    (server-alive-count-max . 3)))
+        (ssh-match '(exec \"grep key secret.txt\")
+                   '((compression . #t))))))
+@end lisp
+
+will turn into this:
+
+@example
+Host savannah
+  Compression no
+  Ciphers 3des-cbc aes256-ctr
+  IdentityFile ~/.ssh/keys.d/id_rsa
+  ServerAliveCountMax 3
+Match exec \"grep key secret.txt\"
+  Compression yes
+@end example"))
+
+(define (add-ssh-configuration config)
+  `(("ssh/config"
+     ,(mixed-text-file "ssh-config"
+                       (serialize-configuration
+                        config
+                        home-ssh-configuration-fields)))))
+
+(define (add-ssh-packages config)
+  (list (home-ssh-configuration-package config)))
+
+(define home-ssh-service-type
+  (service-type (name 'home-ssh)
+                (extensions
+                 (list (service-extension
+                        home-files-service-type
+                        add-ssh-configuration)
+                       (service-extension
+                        home-profile-service-type
+                        add-ssh-packages)))
+                (default-value (home-ssh-configuration))
+                (description "Install and configure SSH")))
+
+(define (generate-home-ssh-documentation)
+  (generate-documentation
+   `((home-ssh-configuration
+      ,home-ssh-configuration-fields
+      (ssh-host ssh-host)
+      (ssh-match ssh-match))
+     (ssh-host ,ssh-host-fields)
+     (ssh-match ,ssh-match-fields))
+   'home-ssh-configuration))
