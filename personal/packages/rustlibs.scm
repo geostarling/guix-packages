9d3108890a7de1fb33c3149ce1ff2c83e1ee0f80
Jiri Spacek <spacekj3@gmail.com>
2022-02-12 22:27:33 +0100

    rustlibs: fix import

--- personal/packages/rustlibs.scm
+++ personal/packages/rustlibs.scm
@@ -42,6 +42,7 @@
   #:use-module (gnu packages perl)
   #:use-module (gnu packages tcl)
   #:use-module (gnu packages linux)
+  #:use-module (gnu packages crates-io)
   #:use-module (gnu packages ninja)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages search)
@@ -70,3 +71,49 @@
      "This package provides a macro to generate structures which behave like bitflags.
  ")
    (license #f)))
+
+
+(define-public rust-argparse-0.2
+  (package
+    (name "rust-argparse")
+    (version "0.2.2")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "argparse" version))
+        (file-name (string-append name "-" version ".tar.gz"))
+        (sha256
+          (base32 "0iqy2jkifwq0azrrh26qjssp7sknjylycq35jkalzb744xcbz3iz"))))
+    (build-system cargo-build-system)
+    (arguments `(#:skip-build? #t))
+    (home-page "http://github.com/tailhook/rust-argparse")
+    (synopsis "Powerful command-line argument parsing library")
+    (description "Powerful command-line argument parsing library")
+    (license license:expat)))
+
+(define-public rust-unshare-0.7
+  (package
+    (name "rust-unshare")
+    (version "0.7.0")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "unshare" version))
+        (file-name (string-append name "-" version ".tar.gz"))
+        (sha256
+          (base32 "16vvivmj7r64ymxc0grqgv4bjxmdaiv26j57z24xl7iaanas5v8w"))))
+    (build-system cargo-build-system)
+    (arguments
+      `(#:cargo-inputs
+        (("rust-libc" ,rust-libc-0.2)
+         ("rust-nix" ,rust-nix-0.20))
+        #:cargo-development-inputs
+        (("rust-argparse" ,rust-argparse-0.2)
+         ("rust-rand" ,rust-rand-0.8))))
+    (home-page "https://github.com/tailhook/unshare")
+    (synopsis
+      "    The low-level interface for linux namespaces (containers)
+")
+    (description
+      "    The low-level interface for linux namespaces (containers)")
+    (license (list license:expat license:asl2.0))))
