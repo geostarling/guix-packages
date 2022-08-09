4151413641626459e1df6bab35128d47fe9b9cbf
Jiri Spacek <spacek@stokorec.cz>
2022-08-09 23:04:12 +0200

    wayfire: update to 0.7.4

--- personal/packages/wayfire.scm
+++ personal/packages/wayfire.scm
@@ -28,9 +28,12 @@
   #:use-module (gnu packages admin)
   #:use-module (gnu packages maths)
   #:use-module (gnu packages file)
+  #:use-module (gnu packages version-control)
   #:use-module (gnu packages c)
   #:use-module (gnu packages commencement)
   #:use-module (guix utils)
+  #:use-module (ice-9 match)
+
 
   #:use-module (gnu packages check)
   #:use-module (gnu packages gcc)
@@ -52,6 +55,7 @@
   #:use-module (gnu packages xdisorg)
   #:use-module (gnu packages video)
   #:use-module (gnu packages image)
+  #:use-module (gnu packages xml)
   #:use-module (gnu packages gstreamer)
   #:use-module (gnu packages llvm)
   #:use-module (gnu packages ncurses)
@@ -70,82 +74,118 @@
   #:use-module (gnu packages cmake)
   #:use-module (gnu packages freedesktop))
 
-(define-public my-wlroots
+(define-public wayland-protocols-24
   (package
-    (name "wlroots")
-    (version "0.8.0")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/swaywm/wlroots.git")
-             (commit version)))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "0w3lfzczry5zk7x2kxdb8anb4bymsby6h1i2gc0w2f8hgx4m2b9x"))))
+    (name "wayland-protocols")
+    (version "1.24")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "https://wayland.freedesktop.org/releases/"
+                    "wayland-protocols-" version ".tar.xz"))
+              (sha256
+               (base32
+                "1hlb6gvyqlmsdkv5179ccj07p04cn6xacjkgklakbszczv7xiw5z"))))
+    (build-system meson-build-system)
+    (inputs
+     (list wayland))
+    (native-inputs
+     (list pkg-config python))
+    (synopsis "Wayland protocols")
+    (description "Wayland-Protocols contains Wayland protocols that add
+functionality not available in the Wayland core protocol.  Such protocols either
+add completely new functionality, or extend the functionality of some other
+protocol either in Wayland core, or some other protocol in wayland-protocols.")
+    (home-page "https://wayland.freedesktop.org")
+    (properties
+     '((release-monitoring-url
+        . "https://wayland.freedesktop.org/releases.html")))
+    (license license:expat)))
+
+
+(define-public libdrm-109
+  (package
+    (name "libdrm")
+    (version "2.4.109")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "https://dri.freedesktop.org/libdrm/libdrm-"
+                    version ".tar.xz"))
+              (sha256
+               (base32
+                "09kzrdsd14zr0i3izvi5mck4vqccl3c9hr84r9i4is0zikh554v2"))))
     (build-system meson-build-system)
     (arguments
-     `(#:configure-flags '("-Dlogind-provider=elogind")
-       #:phases
-       (modify-phases %standard-phases
-         (add-before 'configure 'hardcode-paths
-           (lambda* (#:key inputs #:allow-other-keys)
-             (substitute* "xwayland/xwayland.c"
-               (("Xwayland") (string-append (assoc-ref inputs
-                                                       "xorg-server-xwayland")
-                                            "/bin/Xwayland")))
-             #t)))))
-    (inputs `(("elogind" ,elogind)
-              ("eudev" ,eudev)
-              ("libinput" ,libinput)
-              ("libxkbcommon" ,libxkbcommon)
-              ("mesa" ,mesa)
-              ("pixman" ,pixman)
-              ("wayland" ,wayland)
-              ("xorg-server-xwayland" ,xorg-server-xwayland)))
-    (native-inputs `(("ffmpeg" ,ffmpeg)
-                     ("libcap" ,libcap)
-                     ("libpng" ,libpng)
-                     ("pkg-config" ,pkg-config)
-                     ("wayland-protocols" ,wayland-protocols)))
-    (home-page "https://github.com/swaywm/wlroots")
-    (synopsis "Pluggable, composable, unopinionated modules for building a
-Wayland compositor")
-    (description "wlroots is a set of pluggable, composable, unopinionated
-modules for building a Wayland compositor.")
-    (license license:expat)))  ; MIT license
+     `(#:configure-flags
+       '(,@(match (%current-system)
+             ((or "armhf-linux" "aarch64-linux")
+              '("-Dexynos=true"
+                "-Domap=true"
+                "-Detnaviv=true"
+                "-Dtegra=true"
+                "-Dfreedreno-kgsl=true"))
+             (_ '())))
+
+       #:phases (modify-phases %standard-phases
+                  (replace 'check
+                    (lambda* (#:key tests? #:allow-other-keys)
+                      (when tests?
+                        (invoke "meson" "test" "--timeout-multiplier" "5")))))))
+    (propagated-inputs
+     (list libpciaccess))
+    (native-inputs
+     (list pkg-config))
+    (home-page "https://dri.freedesktop.org/wiki/")
+    (synopsis "Direct rendering userspace library")
+    (description "The Direct Rendering Infrastructure, also known as the DRI,
+is a framework for allowing direct access to graphics hardware under the
+X Window System in a safe and efficient manner.  It includes changes to the
+X server, to several client libraries, and to the kernel (DRM, Direct
+Rendering Manager).  The most important use for the DRI is to create fast
+OpenGL implementations providing hardware acceleration for Mesa.
+Several 3D accelerated drivers have been written to the DRI specification,
+including drivers for chipsets produced by 3DFX, AMD (formerly ATI), Intel
+and Matrox.")
+    (license license:x11)))
 
 ;; NOTE: I don't really get it. It should not be neccessary to specify many of dependencies (deps of wlroots for example) because they should be transitively resolved (eg. libinput is definitely dep of wlroots so why we have to specify it again?) is it meson-related?
 (define-public wayfire
   (package
     (name "wayfire")
-    (version "0.3.0")
+    (version "0.7.4")
     (source (origin
               (method url-fetch)
-              (uri (string-append "https://github.com/WayfireWM/wayfire/releases/download/" version "/wayfire-0.3.tar.xz"))
+              (uri (string-append "https://github.com/WayfireWM/wayfire/releases/download/v" version "/wayfire-" version ".tar.xz"))
               (sha256
                (base32
-                "11py8f8vknqw1kmlj4g9p6b0zbrjyd30bsr4ssyxxnih23c4xi89"))))
+                "0kaiy2kk8yc9zl6amdgv99chjwpfg7wrk54z7l1d8yqd6bvpbqw9"))))
     (build-system meson-build-system)
     (arguments
      `(#:phases
        (modify-phases %standard-phases
-                      (add-before 'configure 'fixgcc7
+                      (add-before 'configure 'fix-meson-build
                                   (lambda _
-                                    (unsetenv "C_INCLUDE_PATH")
-                                    (unsetenv "CPLUS_INCLUDE_PATH"))))))
+                                    (substitute* "src/meson.build"
+                                      (("^.*/bin/sh.*$")
+                                       "cxx_flags_asan = run_command('sh', '-c', 'echo $CXXFLAGS $CPPFLAGS | grep fsanitize')\n")))))))
     (native-inputs `(("pkg-config" ,pkg-config)
                      ("cmake" ,cmake)
-                     ("gcc" ,gcc-7)))
-    (inputs `(("wlroots" ,my-wlroots)
+                     ("bash" ,bash)
+                     ("git" ,git)
+                     ("glibc" ,glibc)
+                     ("gcc" ,gcc)))
+    (inputs `(("wlroots" ,wlroots)
               ("glm" ,glm)
               ("libcap" ,libcap) ;; dep of wlroots
               ("elogind" ,elogind) ;; dep of wlroots
               ("wayland" ,wayland)
-              ("wayland-protocols" ,wayland-protocols)
+              ("wayland-protocols" ,wayland-protocols-24)
               ("cairo" ,cairo)
-              ("libdrm" ,libdrm)
+              ("libdrm" ,libdrm-109)
               ("mesa" ,mesa)
+              ("pango" ,pango)
+              ("libxml2" ,libxml2)
               ("libevdev" ,libevdev)
               ("libinput" ,libinput)
               ("libxkbcommon" ,libxkbcommon)))
@@ -161,7 +201,7 @@ modules for building a Wayland compositor.")
 (define-public wf-config
   (package
     (name "wf-config")
-    (version "0.3")
+    (version "0.7.1")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://github.com/WayfireWM/wf-config/releases/download/v" version "/wf-config-" version  ".tar.xz"))
@@ -203,7 +243,7 @@ modules for building a Wayland compositor.")
 (define-public wf-shell
   (package
     (name "wf-shell")
-    (version "0.3")
+    (version "0.7.0")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://github.com/WayfireWM/wf-shell/releases/download/" version "/wf-shell-" version  ".tar.xz"))
@@ -234,7 +274,7 @@ modules for building a Wayland compositor.")
               ("libinput" ,libinput)
               ("libxkbcommon" ,libxkbcommon)
               ("glm" ,glm)
-              ("wlroots" ,my-wlroots)))
+              ("wlroots" ,wlroots)))
 
     (home-page
      "https://goodies.xfce.org/projects/panel-plugins/xfce4-sensors-plugin")
@@ -272,7 +312,6 @@ modules for building a Wayland compositor.")
 ;;     (description
 ;;      "")
 ;;     (license (list license:gpl2+ license:lgpl2.0+))))
-
 
 
 (define-public albert
